25800dba1d629e5ebfe44d38bcd2883f8712f1e5
akshat <axxatagrawal@gmail.com>
2025-03-24 11:18:24 +0530

    fix: update webhook URL in setupSmartLeadWebhook function

--- src/utility/webhookEvent.js
+++ src/utility/webhookEvent.js
@@ -18,7 +18,7 @@ export async function setupSmartLeadWebhook(campaignId) {
       data: {
         id: null,
         name: "Email Activity Tracking Webhook",
-        webhook_url: `https://stir-email-outreach.onrender.com/api/webhook/smartlead`,
+        webhook_url: `http://104.131.101.181:3007/api/webhook/smartlead`,
         event_types: ['EMAIL_SENT', 'EMAIL_REPLY', 'LEAD_UNSUBSCRIBED'],
         categories: [ "Interested"]
       },
